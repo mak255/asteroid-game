c3e7f7064bd40748da79dd35f9a51c1a8e88d653
Mayank Tiwari <makespark@gmail.com>
2026-01-14 17:53:12 +0530

    [upgrade] added the shooting for the player without  zigrush

--- shot.py
+++ shot.py
@@ -1,12 +1,13 @@
 import pygame 
 from circleshape import CircleShape
+from constants import SHOT_RADIUS, LINE_WIDTH
 
 class Shot(CircleShape):
-    def __init__(self, x, y, radius):
-        super().__init__(x, y, radius)
+    def __init__(self, x, y):
+        super().__init__(x, y, SHOT_RADIUS)
 
     def draw(self, screen):
-        pass
+        pygame.draw.circle(screen, "white", self.position, SHOT_RADIUS, LINE_WIDTH)
 
     def update(self, dt):
-        pass
+        self.position += (self.velocity * dt)
